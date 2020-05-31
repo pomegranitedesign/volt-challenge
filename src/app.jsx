11f2b3c55e6e8c63a3e016c588504c857d57c6dd
Dmitiry Shin <dimashin9@gmail.com>
2020-05-31 13:22:31 -0400

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import 'react-select/dist/react-select.css'
+
+render(
+	<h1>Place your application here</h1>,
+	document.getElementById('app-root')
+)
